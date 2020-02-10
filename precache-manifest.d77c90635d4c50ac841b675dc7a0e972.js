120def69dbb3ad597a5717437aa33127c50c6a89
Valentin <avelnightroude@gmail.com>
2020-02-10 19:35:35 +0500

    Updates

---
+++ precache-manifest.d77c90635d4c50ac841b675dc7a0e972.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c030d6c0ceb71e99050fa0a533dec2",
+    "url": "/FathersSite/index.html"
+  },
+  {
+    "revision": "fdb561bb46c861645298",
+    "url": "/FathersSite/static/css/2.01194d94.chunk.css"
+  },
+  {
+    "revision": "29630bacce5c54db0d2c",
+    "url": "/FathersSite/static/css/main.a598d861.chunk.css"
+  },
+  {
+    "revision": "fdb561bb46c861645298",
+    "url": "/FathersSite/static/js/2.2cbdc89e.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/FathersSite/static/js/2.2cbdc89e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29630bacce5c54db0d2c",
+    "url": "/FathersSite/static/js/main.c166fa53.chunk.js"
+  },
+  {
+    "revision": "c8b0cb1a60aecab0b6a0",
+    "url": "/FathersSite/static/js/runtime-main.650db936.js"
+  },
+  {
+    "revision": "14226958a872123ebbcd32ecbfc74e27",
+    "url": "/FathersSite/static/media/AleksChernov.14226958.jpg"
+  },
+  {
+    "revision": "6f1f88d70b513bdc9be2dbbe0d4292ff",
+    "url": "/FathersSite/static/media/DmitriySolovyev.6f1f88d7.jpg"
+  },
+  {
+    "revision": "d996e1aebb63254fb0daa8202084eee4",
+    "url": "/FathersSite/static/media/MikhailIvaniv.d996e1ae.jpg"
+  },
+  {
+    "revision": "96b50bafb1eadc9f15d98bc92dd5b249",
+    "url": "/FathersSite/static/media/OKy-b6lF06I.96b50baf.jpg"
+  },
+  {
+    "revision": "a3e4333ddb7aa6f24521d84585541b59",
+    "url": "/FathersSite/static/media/RuslanMagomedov.a3e4333d.jpg"
+  },
+  {
+    "revision": "d2b5b70f73f1b9eb8ba5e6c48bd20796",
+    "url": "/FathersSite/static/media/VasilyPeskov.d2b5b70f.jpg"
+  },
+  {
+    "revision": "b21d8235eefbd6d1c4b5cff09cb8cae2",
+    "url": "/FathersSite/static/media/boxing.b21d8235.jpg"
+  },
+  {
+    "revision": "02284192bd07ace0850aa199f1c2c8aa",
+    "url": "/FathersSite/static/media/fathers-mobile.02284192.jpg"
+  },
+  {
+    "revision": "7627192aa5640e1551d318ea854d5b40",
+    "url": "/FathersSite/static/media/fathers.7627192a.jpg"
+  },
+  {
+    "revision": "27f8045680abfab5fcddf33dbfabe0c1",
+    "url": "/FathersSite/static/media/grappling.27f80456.jpg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/FathersSite/static/media/iconfinder_menu-alt_134216.86595150.svg"
+  },
+  {
+    "revision": "67f4c48f6991b9a1d8d1fec7f0350faf",
+    "url": "/FathersSite/static/media/inst.67f4c48f.svg"
+  },
+  {
+    "revision": "d20a8d5fba802187ee23a21363f89d2e",
+    "url": "/FathersSite/static/media/jiujitsu.d20a8d5f.jpg"
+  },
+  {
+    "revision": "9aea9f480e9a0d225a1e9616ec12c515",
+    "url": "/FathersSite/static/media/karate-1343889_1920.9aea9f48.jpg"
+  },
+  {
+    "revision": "2b7c48a767e22546941d70afc9f27ff0",
+    "url": "/FathersSite/static/media/map-marker-alt-solid.2b7c48a7.svg"
+  },
+  {
+    "revision": "ad5fb85475ff7480a30657bfb35f0fc6",
+    "url": "/FathersSite/static/media/mauthai.ad5fb854.jpg"
+  },
+  {
+    "revision": "716d8d96f1ddd9615ea4565f8d844583",
+    "url": "/FathersSite/static/media/mma.716d8d96.jpg"
+  },
+  {
+    "revision": "bacc2d0a2820b3626f60e35deada6d0c",
+    "url": "/FathersSite/static/media/phone-alt-solid.bacc2d0a.svg"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/FathersSite/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/FathersSite/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/FathersSite/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/FathersSite/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/FathersSite/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/FathersSite/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/FathersSite/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/FathersSite/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/FathersSite/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/FathersSite/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/FathersSite/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/FathersSite/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/FathersSite/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/FathersSite/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/FathersSite/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/FathersSite/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/FathersSite/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/FathersSite/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/FathersSite/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/FathersSite/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/FathersSite/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/FathersSite/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/FathersSite/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/FathersSite/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "be10a0cc4cda29120007715abc1375c3",
+    "url": "/FathersSite/static/media/time.be10a0cc.svg"
+  },
+  {
+    "revision": "60e31664870895cd8d5af4fd2137f153",
+    "url": "/FathersSite/static/media/vk-brands.60e31664.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.7d26c6b813e1ac6b300e907a49574ba8.js"
+  "/FathersSite/precache-manifest.d77c90635d4c50ac841b675dc7a0e972.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FathersSite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
